fa0dbb7e8137274dc8633b415bf0d165ecbf8862
ckatjrdavis <ckatjrdavis@gmail.com>
2022-01-23 15:23:55 -0600

    added edit.html and views.edit function for editing existing heroes, added delete function to delete existing heroes

--- superhero_project/superheroes/views.py
+++ superhero_project/superheroes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.template import context
 from django.urls import reverse
 from .models import Superhero
 
@@ -16,7 +17,8 @@ def index(request):
 def details(request, hero_id):
     single_hero = Superhero.objects.get(pk=hero_id)
     context = {
-        'single_hero': single_hero
+        'single_hero': single_hero,
+        'hero_id': hero_id
     }
     return render(request, 'superheroes/details.html', context)
 
@@ -35,3 +37,24 @@ def create(request):
         # return to index
     else:
         return render(request, 'superheroes/create.html')
+
+def edit(request, hero_id):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        alter_ego = request.POST.get('alter_ego')
+        primary = request.POST.get('primary')
+        secondary = request.POST.get('secondary')
+        catchphrase = request.POST.get('catchphrase')
+        Superhero.objects.filter(pk=hero_id).update(name=name, alter_ego=alter_ego, primary_ability=primary, secondary_ability=secondary, catch_phrase=catchphrase)
+        return HttpResponseRedirect(reverse('superheroes:index'))
+    else:
+        single_hero = Superhero.objects.get(pk=hero_id)
+        context = {
+            'single_hero': single_hero,
+            'hero_id': hero_id
+        }
+        return render(request, 'superheroes/edit.html', context)
+
+def delete(request, hero_id):
+    Superhero.objects.filter(pk=hero_id).delete()
+    return HttpResponseRedirect(reverse('superheroes:index'))
